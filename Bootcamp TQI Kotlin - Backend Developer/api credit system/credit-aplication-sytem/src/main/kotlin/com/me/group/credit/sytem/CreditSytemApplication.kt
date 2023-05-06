7d7f34c7dcfb2eb21bb155bc6dc70b03f55ed063
R0nald0 <ronaldo-172010@hotmail.com>
2023-05-06 15:43:34 -0300

    iniciado projeto application credit system dio

---
+++ Bootcamp TQI Kotlin - Backend Developer/api credit system/credit-aplication-sytem/src/main/kotlin/com/me/group/credit/sytem/CreditSytemApplication.kt
@@ -0,0 +1,11 @@
+package com.me.group.credit.sytem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CreditSytemApplication
+
+fun main(args: Array<String>) {
+	runApplication<CreditSytemApplication>(*args)
+}
